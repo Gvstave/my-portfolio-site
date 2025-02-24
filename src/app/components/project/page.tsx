9f31ffe7e759689ebd102ccdaaeddf9db8ff68b4
Gvstave <ilungagustave73@gmail.com>
2025-02-25 00:02:48 +0200

    added url links

--- src/app/components/project/page.tsx
+++ src/app/components/project/page.tsx
@@ -1,12 +1,12 @@
-"use client";
+import { Github, Radio } from "lucide-react";
 
 interface itemProps {
   bgImage: React.ReactNode;
   name: string;
   details: string;
   stack: string[];
-  code: React.ReactNode;
-  preview: React.ReactNode;
+  codeUrl: string; 
+  pageUrl: string;
 }
 
 const Projects: React.FC<itemProps> = ({
@@ -14,31 +14,44 @@ const Projects: React.FC<itemProps> = ({
   name,
   details,
   stack,
-  code,
-  preview,
+  codeUrl,
+  pageUrl,
 }) => {
   return (
-    <div className="mt-10 bg-white pb-1 border-2 border-black-900 border-solid">
-      <div className="border-b-2">{bgImage}</div>
-      <div className="text-left m-5">
-        <h4 className="font-bold text-md mb-3">{name}</h4>
-        <p>{details}</p>
-      </div>
-      <div className="m-5">
-        <ul className="flex flex-row gap-4">
-          {stack.map((item, index) => (
-            <li
-              key={index}
-              className="px-2 bg-gray-200 border-red-400 rounded"
-            >
-              {item}
-            </li>
-          ))}
-        </ul>
-      </div>
-      <div className="flex flex-row gap-4 items-center m-5">
-        <a className="flex flex-row">{code}Code</a>
-        <a className="flex flex-row">{preview}Live Preview</a>
+    <div className="text-gray-200 mt-10 bg-[#000] border-2 border-gray-800 shadow-lg shadow-[#111] rounded-lg">
+      <div>{bgImage}</div>
+      <div className="flex flex-col gap-5 p-5">
+        <div className="text-left">
+          <h4 className="font-bold text-lg text-white">{name}</h4>
+          <p>{details}</p>
+        </div>
+        <div>
+          <ul className="flex flex-row gap-4">
+            {stack.map((item, index) => (
+              <li key={index} className="px-2 border-2 border-gray-800 rounded">
+                {item}
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="flex flex-row gap-8 items-center text-green-500">
+          <a
+            href={codeUrl}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex flex-row gap-1 hover:text-green-600"
+          >
+            <Github /> Code
+          </a>
+          <a
+            href={pageUrl}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex flex-row gap-1 hover:text-green-600"
+          >
+            <Radio /> Live Preview
+          </a>
+        </div>
       </div>
     </div>
   );

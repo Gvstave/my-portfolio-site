dfc125dacad067ccfadf298c9cec2d0b6a5842f7
Gvstave <ilungagustave73@gmail.com>
2025-08-17 11:33:12 +0200

    initial commit

--- src/sections/project.tsx
+++ src/sections/project.tsx
@@ -3,7 +3,7 @@ export const Project = () => {
         <div className="bg-[#000] px-[72px] pt-64 pb-32 max-md:px-[10px] max-md:py-5">
             <div>
                 <h2 className="text-[#8491a0] mb-2 text-[16px] font-bold leading-[150%] ">My Projects</h2>
-                <h2 className="max-w-[780px] text-white text-[55px] leading-[110%] font-medium">Work that I’ve done for the past 3 years</h2>
+                <h2 className="max-w-[780px] text-white text-[55px] leading-[110%] font-medium">Work that I’ve worked on for the past 3 years</h2>
             </div>
             <div className="flex max-lg:flex-col gap-10">
                 {

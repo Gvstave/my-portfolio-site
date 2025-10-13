dc006fa5aba436a0ae81bf84400a7d54fac5b4af
gvstave <ilungagustave73@gmail.com>
2025-10-13 10:39:27 +0200

    feat/fix background color of the project component

--- src/sections/project.tsx
+++ src/sections/project.tsx
@@ -7,7 +7,7 @@ export const Project = () => {
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="text-center mb-16">
                     <h2 className="text-theme-secondary mb-4 text-[16px] font-bold leading-[150%] uppercase tracking-wider">My Projects</h2>
-                    <h2 className="bg-green-500 text-theme-primary text-[48px] max-md:text-[36px] leading-[110%] font-medium max-w-4xl mx-auto">What I've worked on in the past 3 years</h2>
+                    <h2 className="text-theme-primary text-[48px] max-md:text-[36px] leading-[110%] font-medium max-w-4xl mx-auto">What I've worked on in the past 3 years</h2>
                 </div>
                 <div className="w-full grid md:grid-cols-2 mx-auto sm:grid-cols-1 gap-10">
                     {
